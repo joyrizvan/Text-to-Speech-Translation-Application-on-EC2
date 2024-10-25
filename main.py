37f58c6d07754ef3c2173c6103ac6c4b69c793ea
Rizvan Nahif <joyrizvan@gmail.com>
2024-10-25 01:28:33 -0400

    dummy code 3

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-    app.debug(True)
+    #app.debug(True)
