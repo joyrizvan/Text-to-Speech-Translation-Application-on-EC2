8765a5741acf417f9df88b5bf9109b0227f81156
Rizvan Nahif <joyrizvan@gmail.com>
2024-10-25 21:16:27 -0400

    audio return 2

--- run.py
+++ run.py
@@ -28,6 +28,8 @@ def main():
 
         return render_template("index_a.html", input=input, lang=selected_lang, translate=translate, audio_url=audio_url)
 
+#Insert the line below to to run on Cloud9    
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 
 if __name__ == '__main__':
     app.run()
